6cec710334498b68750a81680d9f1f2708507e4d
checkRaiseOnCloud <baijiayu01@gmail.com>
2023-05-29 23:58:10 -0700

    print

--- Parser.hpp
+++ Parser.hpp
@@ -17,15 +17,27 @@
 #include <memory>
 #include <vector>
 #include <map>
+#include <iostream>
 
 using namespace std;
 
 namespace {
 
+static void printIndentation(int level){
+    for(int i = 0; i < level; i++) {
+        std::cout << "----";
+    }
+}
+
 /* Base class for expression nodes */
 class ASTBaseExpr {
 public:
     virtual ~ASTBaseExpr() = default;
+
+    virtual void debugMessage(int level) {
+        printIndentation(level);
+        std::cout << "ASTBaseExpr" << std::endl;
+    }
 };
 
 /* Number Expression */
@@ -33,6 +45,11 @@ class ASTNumberExpr : public ASTBaseExpr {
     double value;
 public:
     ASTNumberExpr(double value) : value(value) {};
+
+    void debugMessage(int level) {
+        printIndentation(level);
+        std::cout << "Number(" << to_string(value) << ")" << std::endl;
+    }
 };
 
 /* Variable Expression */
@@ -40,6 +57,11 @@ class ASTVariableExpr : public ASTBaseExpr {
     string identifier;
 public:
     ASTVariableExpr(string identifier) : identifier(identifier) {};
+
+    void debugMessage(int level) {
+        printIndentation(level);
+        std::cout << "Variable(" << identifier << ")" << std::endl;
+    }
 };
 
 /* Binary Expression */
@@ -50,6 +72,17 @@ class ASTBinaryExpr : public ASTBaseExpr {
 public:
     ASTBinaryExpr(char op, unique_ptr<ASTBaseExpr> LHS, unique_ptr<ASTBaseExpr> RHS) :
         op(op), LHS(std::move(LHS)), RHS(std::move(RHS)) {};
+    
+    void debugMessage(int level) {
+        printIndentation(level);
+        std::cout << "BinaryOperator(" << op << ")" << std::endl;
+        printIndentation(level);
+        std::cout << "BinaryLHS(" << op << ")" << std::endl;
+        LHS->debugMessage(level + 1);
+        printIndentation(level);
+        std::cout << "BinaryRHS(" << op << ")" << std::endl;
+        RHS->debugMessage(level + 1);
+    }
 };
 
 /* Function Prototype (Declaration) Expression */
@@ -58,6 +91,15 @@ class ASTProtoExpr : public ASTBaseExpr {
     vector<string> args;
 public:
     ASTProtoExpr(string name, vector<string> args) : name(std::move(name)), args(std::move(args)) {};
+
+    void debugMessage(int level) {
+        printIndentation(level);
+        std::cout << "Prototype(" << name << ") Args:(";
+        for(auto &arg: args){
+            std::cout << arg << ", ";
+        }
+        std::cout << std::endl;
+    }
 };
 
 /* Function Definition Expression */
@@ -67,6 +109,14 @@ class ASTFunctionExpr : public ASTBaseExpr {
 public:
     ASTFunctionExpr(unique_ptr<ASTProtoExpr> prototype, unique_ptr<ASTBaseExpr> body)
     : prototype(std::move(prototype)), body(std::move(body)) {};
+
+    void debugMessage(int level) {
+        printIndentation(level);
+        std::cout << "Function" << std::endl;
+        prototype->debugMessage(level + 1);
+        body->debugMessage(level + 1);
+    }
+
 };
 
 /* Function Call Expression */
@@ -76,6 +126,15 @@ class ASTCallExpr : public ASTBaseExpr {
 public:
     ASTCallExpr(string callee, vector<unique_ptr<ASTBaseExpr>> arguments)
     : callee(std::move(callee)), arguments(std::move(arguments)) {};
+
+    void debugMessage(int level) {
+        printIndentation(level);
+        std::cout << "Call(" << callee << ") Args:(";
+        for(auto &arg: arguments){
+            std::cout << arg << ", ";
+        }
+        std::cout << std::endl;
+    }
 };
 
 } // anonymous namespace
